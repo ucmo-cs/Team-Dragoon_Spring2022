5f1faf08c95394b6414ae0180f2caf6d12526a3a
SMA75740 <89264985+SMA75740@users.noreply.github.com>
2022-02-16 22:34:37 -0600

    Added Ninja with Skeleton
    Created skeleton with bone weights for animating walk cycles within Unity itself

--- SeniorProject/Assets/Scripts/DoorManager.cs
+++ SeniorProject/Assets/Scripts/DoorManager.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DoorManager : MonoBehaviour
 {
+    //can change scene switching system later as it is a little
+    //more complex to implement refrencing the actual scene
+    //
+    //this link has a solution that seems to work, will look at it later
+    //https://answers.unity.com/questions/605184/scene-as-a-variable.html?page=1&pageSize=5&sort=votes
+
+    public string sceneName;
+    private Vector2 doorExitPos;
+    public float exitPosX;
+    public float exitPosY;
+
+    //add private variable to save the character that enters door
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        doorExitPos.x = exitPosX;
+        doorExitPos.y = exitPosY;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Door/Player collision");
+            SceneManager.LoadScene(sceneName);
+            collision.gameObject.transform.position = doorExitPos;
+        }
     }
 }
