e771109f673312266f0c8a97e10f71fb722b9d38
Cody Bruckerhoff <codybruckerhoff@gmail.com>
2022-02-17 14:24:03 -0600

    Battle Scene Start Up
    Once the battle scene starts will create player prefabs in certain areas. Turns not implemented yet. Following Brackely tutorial

---
+++ SeniorProject/Assets/Scripts/unit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unit : MonoBehaviour
+{
+    public string unitName;
+    public int unitLevel;
+
+    public int damage;
+
+    public int maxHP;
+    public int currentHP;
+}
